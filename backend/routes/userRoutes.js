b077cba4d4ab9b52b1654923f7874dfda52f8379
Joshua Owusu <joshbillion1@gmail.com>
2023-05-22 21:45:29 +0000

    Commit auto_update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = expressRouter();
-import { authUser } from './controller/auth'
+import { authUser } from '../controllers/userController'
 
 router.post('/auth', authUser)
 

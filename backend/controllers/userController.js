9dbea94afe9f0e5eaff1d4a259ff7cea7e51af22
Joshua Owusu <joshbillion1@gmail.com>
2023-05-23 15:29:17 +0000

    Commit auto_update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,10 +6,17 @@ import asyncHandler from 'express-async-handler'
 
 const authUser = asyncHandler(async (req, res) => {
 
-    res.status(401);
-    throw new Error('Error comming from client side')
+    // res.status(401);
+    // throw new Error('Some went wrong from the client side');
 
     res.status(200).json({ message : 'User Authenticated'});
+});
+
+const authUser = asyncHandler(async (req, res) => {
+  // res.status(401);
+  // throw new Error('Some went wrong from the client side');
+
+  res.status(200).json({ message: "User Authenticated" });
 });
 
 export { authUser };

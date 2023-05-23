508e06837a7ff310b1f065e3882021deeb5caa37
Joshua Owusu <joshbillion1@gmail.com>
2023-05-23 12:46:18 +0000

    Commit auto_update

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import userRoutes from './routes/UserRoutes.js';
 
 const app = express();
 
+app.use(./api/users)
+
 app.get('/', (req, res) => res.send('Server is ready'));
 
 app.listen(port, () => console.log(`Server Started On port ${port}`));

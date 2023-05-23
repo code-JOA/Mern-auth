9684d2a5d53657319633af631f9dcca0a548282b
Joshua Owusu <joshbillion1@gmail.com>
2023-05-23 14:49:14 +0000

    Commit auto_update

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ app.use('./api/users' , userRoutes);
 
 app.get('/', (req, res) => res.send('Server is ready'));
 
-app.use(notFound)
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server Started On port ${port}`));
 

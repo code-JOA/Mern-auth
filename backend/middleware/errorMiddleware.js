87b5c0bd84ad0a9b4874bad7888b36ac803bf86a
Joshua Owusu <joshbillion1@gmail.com>
2023-05-23 14:18:02 +0000

    Commit auto_update

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -5,5 +5,6 @@ const notFound = ( req, res, Next ) => {
 }
 
 const errorHandler = (req, res, next) => {
-    letStatusCode = res.statusCode === 200 ? 500 : res.statusCode
+    letStatusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let message = err.message;
 }

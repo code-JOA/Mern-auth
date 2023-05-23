fa1eba2580f272ed563f6e0e170993ea46cd8392
Joshua Owusu <joshbillion1@gmail.com>
2023-05-23 22:20:06 +0000

    Commit auto_update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect()   
-    }   catch (error){
+        const conn = await mongoose.connect(process.env.mongo_URI)   
+    }   catch (error) {
 
     }
 }

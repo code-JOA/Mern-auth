a061594d9ac0b869b6c45f4a520a51fde1bb009f
Joshua Owusu <joshbillion1@gmail.com>
2023-05-23 13:01:33 +0000

    Commit auto_update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,12 @@
 // @desc Auth user/ set token
 // route POST /api/users/auth
 // access Public
-import 
+import asyncHandler from 'express-async-handler'
 
 
-const authUser = async (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
 
     res.status(200).json({ message : 'User Authenticated'})
 }
 
-export { authUser } ;
+export { authUser }) ;

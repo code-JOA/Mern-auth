4f7cdfe031b8bcdb773b6e80541eda9cc6ce7ab9
Joshua Owusu <joshbillion1@gmail.com>
2023-05-23 14:13:45 +0000

    Commit auto_update

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,3 +1,5 @@
 const notFound = ( req, res, Next ) => {
-    const error = new Error(`Not Found - ${}`)
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    Next(error);
 }

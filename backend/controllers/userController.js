a81962028f7d97fd054f4848a59c76d876d199f8
Joshua Owusu <joshbillion1@gmail.com>
2023-05-25 10:49:27 +0000

    Commit auto_update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -18,6 +18,7 @@ const authUser = asyncHandler(async (req, res) => {
 // access Public
 
 const registerUser = asyncHandler(async (req, res) => {
+    const req.body()
     res.status(200).json({ message: "User Registered" });
 });
 

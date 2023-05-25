84596222f989ab42e18c34667855a2b5583a53f6
Joshua Owusu <joshbillion1@gmail.com>
2023-05-25 12:31:45 +0000

    Commit auto_update

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,8 @@ const generateToken = (res, userId) => {
     res.cookie('jwt',token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
-        sameSite
+        sameSite: 'strict',
+        maxAge: 30 
     }) 
 
 }

9938e327b3699b7241fc0b8ebead921438cdd0b3
Joshua Owusu <joshbillion1@gmail.com>
2023-05-25 10:52:15 +0000

    Commit auto_update

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ import userRoutes from './routes/UserRoutes.js';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded{extended: true});
+
 app.use('./api/users' , userRoutes);
 
 app.get('/', (req, res) => res.send('Server is ready'));

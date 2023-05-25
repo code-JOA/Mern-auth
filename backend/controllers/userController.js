6aa5d24d9d5600b59132f8930ebc0a492995b3e7
Joshua Owusu <joshbillion1@gmail.com>
2023-05-25 13:04:37 +0000

    Commit auto_update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,7 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
 
       const user = await User.findOne({ email });
 
-      if (user  && await User.matchPassword(user.password)) {
+      if (user  && (await User.matchPassword(password))) {
         generateToken(res, user._id);
         res.status(201).json({
         _id: user._id,

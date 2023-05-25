c2b2ea37206b8f651c085ab21b8710d433a5d4f2
Joshua Owusu <joshbillion1@gmail.com>
2023-05-25 13:14:23 +0000

    Commit auto_update

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,3 +1,6 @@
 import jwt from 'jsonwebtoken';
 import expressAsyncHandler from 'express-async-handler';
-import User from '../models/'
+import User from '../models/userModel.js';
+
+
+const protect = asyncHandler(async => );

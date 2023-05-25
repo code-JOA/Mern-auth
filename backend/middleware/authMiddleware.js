16647f32d8826ff29a5a996f46b6e9a721e0dac7
Joshua Owusu <joshbillion1@gmail.com>
2023-05-25 13:30:41 +0000

    Commit auto_update

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -13,7 +13,8 @@ const protect = asyncHandler(async (req, res, next) => {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
             req.user = await User.findById(decoded.userId).select('-password');
-            
+
+            next(); 
         } catch (error) {
             res.status(401);
             throw new Error('Not authourized, Invalid error')    

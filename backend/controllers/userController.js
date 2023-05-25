67252a580e17d547b63e4c6f42865990c52dd7a9
Joshua Owusu <joshbillion1@gmail.com>
2023-05-25 14:06:16 +0000

    Commit auto_update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -106,6 +106,11 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
 
+    if (req.body.password) {
+      user.password = req
+
+    }
+
   }else{
     res.status(404);
     throw new Error('User Not Found')

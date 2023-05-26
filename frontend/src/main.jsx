d531ffb192046ba71178d85fab78e6b7795b9d4c
Joshua Owusu <joshbillion1@gmail.com>
2023-05-26 18:16:37 +0000

    Commit auto_update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import bootstrap from 'bootstrap';
+// import bootstrap from 'bootstrap';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(

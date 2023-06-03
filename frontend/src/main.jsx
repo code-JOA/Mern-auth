821b146badf6c4223fe5f8b9adac1b0eed71fa7c
Joshua Owusu <joshbillion1@gmail.com>
2023-06-03 16:44:26 +0000

    Commit auto_update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {  } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import bootstrap from 'bootstrap';

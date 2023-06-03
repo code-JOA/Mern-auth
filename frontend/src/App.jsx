12d30875d90426ad6f8bbf28e3648c0d78bc7916
Joshua Owusu <joshbillion1@gmail.com>
2023-06-03 17:06:23 +0000

    Commit auto_update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 // import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 // import Homescreen from './screens/Homescreen';
 

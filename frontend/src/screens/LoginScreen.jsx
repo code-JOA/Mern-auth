4580eea5bfb14337f6390a946a089fb6b98b6195
Joshua Owusu <joshbillion1@gmail.com>
2023-06-04 20:59:51 +0000

    Commit auto_update

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { From, Button, Col, Row } from 'react-bootstrap';
+import { Form, Button, Col, Row } from 'react-bootstrap';
 import FormContainer from '../components/FormContainer';
 
 

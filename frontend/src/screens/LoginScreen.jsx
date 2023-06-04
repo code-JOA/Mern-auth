496b0373e22a84fc6e21f149c204317aa06216b2
Joshua Owusu <joshbillion1@gmail.com>
2023-06-04 20:41:57 +0000

    Commit auto_update

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -37,7 +37,7 @@ const LoginScreen = () => {
           </Form.Control>
         </Form.Group>
 
-        <Button type='submit' variant='primary'>
+        <Button type='submit' variant='primary' className='mt-3'>
             Sign In
         </Button>
       </Form>

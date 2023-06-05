4cc89d7977632c50c92cd2f668439c96b6c52ec6
Joshua Owusu <joshbillion1@gmail.com>
2023-06-05 17:19:12 +0000

    Commit auto_update

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -3,4 +3,7 @@ import { createApi, FetchBaseQuery } from '@reduxjs/toolkit/Query/React';
 
 const baseQuery = fetchBaseQuery({ baseUrl: '' });
 
-const 
+export const apiSlice = createApiSlice({ 
+    baseQuery,
+    tagTypes: ['']
+}) 

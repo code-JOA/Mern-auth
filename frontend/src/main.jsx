b0d1f0b17cd58cc7d8cb68ffe37000e15db9e91c
Joshua Owusu <joshbillion1@gmail.com>
2023-06-05 13:31:42 +0000

    Commit auto_update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,34 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, 
-  createRoutesFromElements,
-  Route, 
-  RouterProvider
-} from 'react-router-dom';
-import App from './App.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./index.css";
-import store from './store';
-import { Provider } from 'react-redux';
-import Homescreen from './screens/Homescreen.jsx';
-import LoginScreen from './screens/LoginScreen.jsx';
-import RegisterScreen from './screens/RegisterScreen.jsx';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { createBrowserRouter, 
+//   createRoutesFromElements,
+//   Route, 
+//   RouterProvider
+// } from 'react-router-dom';
+// import App from './App.jsx';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import "./index.css";
+// import store from './store';
+// import { Provider } from 'react-redux';
+// import Homescreen from './screens/Homescreen.jsx';
+// import LoginScreen from './screens/LoginScreen.jsx';
+// import RegisterScreen from './screens/RegisterScreen.jsx';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<Homescreen />} />
-      <Route path="/login" element={<LoginScreen />} />
-      <Route path="/register" element={<RegisterScreen />} />
-    </Route>
-  )
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<App />}>
+//       <Route index={true} path="/" element={<Homescreen />} />
+//       <Route path="/login" element={<LoginScreen />} />
+//       <Route path="/register" element={<RegisterScreen />} />
+//     </Route>
+//   )
+// );
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  </Provider>
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <Provider store={store}>
+//     <React.StrictMode>
+//       <RouterProvider router={router} />
+//     </React.StrictMode>
+//   </Provider>
 
-);
+// );
+

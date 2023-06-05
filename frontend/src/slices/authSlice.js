11ec83211061e9f64a3299c160139351fa14d1b3
Joshua Owusu <joshbillion1@gmail.com>
2023-06-05 11:00:12 +0000

    Commit auto_update

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -16,8 +16,8 @@ const authSlice = createSlice({
         logout: (state, action) => {
             state.userInfo = null;
             localStorage.removeItem('userInfo');
-        }
-    }
+        },
+    },
 });
 
 export const { setCredentials, logout } =authSlice.actions;

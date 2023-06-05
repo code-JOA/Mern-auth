6efd5da3c3e1c847363c32657e96eec434aae300
Joshua Owusu <joshbillion1@gmail.com>
2023-06-05 09:29:55 +0000

    Commit auto_update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,9 +8,8 @@ import { createBrowserRouter,
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
-// import bootstrap from 'bootstrap';
-import store from './store';
-import { Provider } from 'react-redux';
+// import store from './store';
+// import { Provider } from 'react-redux';
 import Homescreen from './screens/Homescreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
@@ -27,9 +26,9 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  // <Provider store={store}>
     <React.StrictMode>
       <RouterProvider router={router} />
     </React.StrictMode>
-  </Provider>
+  {/* </Provider> */}
 );

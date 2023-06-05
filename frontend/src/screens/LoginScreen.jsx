2872b7658393dc977938a5bce76ee7bfbdf28da5
Joshua Owusu <joshbillion1@gmail.com>
2023-06-05 06:49:54 +0000

    Commit auto_update

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -19,39 +19,36 @@ const LoginScreen = () => {
       <h1>Sign In</h1>
 
       <Form onSubmit={submitHandler}>
-        <Form.Group className='my-2' controlId='email'>
+        <Form.Group className="my-2" controlId="email">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
-            type='email' 
-            placeholder='Enter Email' 
+            type="email"
+            placeholder="Enter Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group className='my-2' controlId='password'>
+        <Form.Group className="my-2" controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
-            type='password'
-            placeholder='Enter Password'
+            type="password"
+            placeholder="Enter Password"
             value={password}
-            onChange={(e) => setPassword(e.target.value) }
+            onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Button 
-        type='submit' 
-        variant='primary' 
-        className='mt-3'>
-            Sign In
+        <Button type="submit" variant="primary" className="mt-3">
+          Sign In
         </Button>
-
-        <Row className='py-3'>
-            <Col >
-                New Customer ? <Link to='/register'>Register</Link>
-            </Col>
-        </Row>
       </Form>
+
+      <Row className="py-3">
+        <Col>
+          New Customer ? <Link to="/register">Register</Link>
+        </Col>
+      </Row>
     </FormContainer>
   )
 }

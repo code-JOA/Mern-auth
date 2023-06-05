774aeff283dfc748cc7e1f91e06954026bed8207
Joshua Owusu <joshbillion1@gmail.com>
2023-06-05 13:21:18 +0000

    Commit auto_update

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
-// import authReducer from './slices/authSlice'
+// import { configureStore } from '@reduxjs/toolkit';
+// // import authReducer from './slices/authSlice'
 
 
 
-const Store = configureStore({
-    reducer: {},
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
-    devTools: true
+// const Store = configureStore({
+//     reducer: {},
+//     middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+//     devTools: true
+// });
+
+// export default store;
+
+
+
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+// import { apiSlice } from "./slices/apiSlice";
+
+const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
 
 export default store;

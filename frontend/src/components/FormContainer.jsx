838af5c3b150f1037d6c72c54f4f3ba23500160e
Joshua Owusu <joshbillion1@gmail.com>
2023-06-05 07:23:29 +0000

    Commit auto_update

--- frontend/src/components/FormContainer.jsx
+++ frontend/src/components/FormContainer.jsx
@@ -6,9 +6,9 @@ const FormContainer = ({ children }) => {
     <Container>
         <Row className='justify-content-md-center mt-5'>
 
-            <col xs={ 12 } md={ 6 } className='card p-5'>
+            <Col xs={ 12 } md={ 6 } className='card p-5'>
                 { children }
-            </col>
+            </Col>
 
         </Row>
     </Container>

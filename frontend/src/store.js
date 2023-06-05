1da28e4c54bf26a98825bf3b372f7aca194a10dd
Joshua Owusu <joshbillion1@gmail.com>
2023-06-05 13:26:29 +0000

    Commit auto_update

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,7 +14,7 @@
 
 
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/authSlice";
+// import authReducer from "./slices/authSlice";
 // import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({

89dfd678b078dd42759354f87ad645f4b5a42b0c
Joshua Owusu <joshbillion1@gmail.com>
2023-06-05 17:24:32 +0000

    Commit auto_update

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,7 @@ import { apiSlice } from "./slices/apiSlice";
 const store = configureStore({
     reducer: {
     auth: authReducer,
-    [apiSlice.reducerPath] = 
+    [ apiSlice.reducerPath ] : apiSlice.reducer ,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
     devTools: true,

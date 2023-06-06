8e833ef6d6efd3a3fd89a52e2fe2494b0f6e1658
Joshua Owusu <joshbillion1@gmail.com>
2023-06-06 13:28:20 +0000

    Commit auto_update

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -4,6 +4,9 @@ const USERS_URL = '/api/users';
 
 export const userApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
-        
+        query: (data) => ({
+            url: `${USERS_URL}/auth`,
+            method: 'POST'
+        })
     })
 })

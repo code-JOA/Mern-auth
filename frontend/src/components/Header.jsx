f9f6ab7d786a6dfeae7fa496012433c17b499131
Joshua Owusu <joshbillion1@gmail.com>
2023-06-07 05:17:42 +0000

    Commit auto_update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -16,7 +16,8 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className='ms-auto'>
-              { userInfo ? () : (
+              {userInfo ? (): (
+                <> 
                 <LinkContainer to='/login'>
                 <Nav.Link>
                   <FaSignInAlt /> Sign In
@@ -28,6 +29,7 @@ const Header = () => {
                   <FaSignOutAlt /> Sign Up
                 </Nav.Link>
               </LinkContainer>
+                </>
 
               ) }
               

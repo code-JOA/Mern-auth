7ef4617990a838139bb76389ea882d37f4d11d13
Joshua Owusu <joshbillion1@gmail.com>
2023-06-07 04:42:56 +0000

    Commit auto_update

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -31,7 +31,7 @@ const LoginScreen = () => {
           dispatch(setCredentials({...res}))
           navigate('/')
         } catch (err) {
-          consol.log(err.data.message);
+          consol.log(err?.data?.message || err.error);
         }
     }
 

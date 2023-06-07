a342f38170ab342209a8882d0141e2d6d361bff9
Joshua Owusu <joshbillion1@gmail.com>
2023-06-07 12:53:38 +0000

    Commit auto_update

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -3,21 +3,31 @@ import { apiSlice } from './apiSlice';
 const USERS_URL = '/api/users';
 
 export const usersApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        login: (builder).mutation({
-            query: (data) => ({
-                url: `${USERS_URL}/auth`,
-                method: 'POST',
-                body: data
-            }),
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    logout: builder.mutation({
+      query: (data) =>
+        data({
+          url: `${USERS_URL}/logout`,
+          method: "POST",
         }),
-        logout: (builder).mutation({
-            query: (data) => data ({
-                url: `${USERS_URL}/logout`,
-                method: 'POST',
-            })
-        })
-    })
-})
+    }),
+  }),
+});
 
 export const { useLoginMutation, useLogoutMutation  } = usersApiSlice;

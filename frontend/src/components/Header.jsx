62aa9486a30ddffa7939c77f483a71c5d923e9a9
Joshua Owusu <joshbillion1@gmail.com>
2023-06-07 05:41:43 +0000

    Commit auto_update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,40 +1,64 @@
 import { Navbar, Nav, Container, NavDropdown, Badge } from "react-bootstrap";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
-import { useSelector, useDispatch } from 'react-redux';
-import { LinkContainer } from 'react-router-bootstrap';
+import { LinkContainer } from "react-router-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
 
 const Header = () => {
-  const {userInfo} = useSelector((state) => state.auth)
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
-            <Navbar.Brand>MERN App</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>MERN Auth</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className='ms-auto'>
+            <Nav className="ms-auto">
               {userInfo ? (
                 <>
-                <NavDropdown title={userInfo.name} ></NavDropdown>
+                  <NavDropdown title={userInfo.name} id="username">
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
                 </>
               ) : (
                 <>
-                <LinkContainer to='/login'>
-                <Nav.Link>
-                  <FaSignInAlt /> Sign In
-                </Nav.Link>
-              </LinkContainer>
-
-              <LinkContainer to='/register'>
-                <Nav.Link>
-                  <FaSignOutAlt /> Sign Up
-                </Nav.Link>
-              </LinkContainer>
-              ) }
-              
+                  <LinkContainer to="/login">
+                    <Nav.Link>
+                      <FaSignInAlt /> Sign In
+                    </Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/register">
+                    <Nav.Link>
+                      <FaSignOutAlt /> Sign Up
+                    </Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -44,75 +68,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
-// import { Navbar, Nav, Container, NavDropdown, Badge } from "react-bootstrap";
-// import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
-// import { LinkContainer } from "react-router-bootstrap";
-// import { useSelector, useDispatch } from "react-redux";
-// import { useNavigate } from "react-router-dom";
-// import { useLogoutMutation } from "../slices/usersApiSlice";
-// import { logout } from "../slices/authSlice";
-
-// const Header = () => {
-//   const { userInfo } = useSelector((state) => state.auth);
-
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-
-//   const [logoutApiCall] = useLogoutMutation();
-
-//   const logoutHandler = async () => {
-//     try {
-//       await logoutApiCall().unwrap();
-//       dispatch(logout());
-//       navigate("/login");
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-
-//   return (
-//     <header>
-//       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-//         <Container>
-//           <LinkContainer to="/">
-//             <Navbar.Brand>MERN Auth</Navbar.Brand>
-//           </LinkContainer>
-//           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//           <Navbar.Collapse id="basic-navbar-nav">
-//             <Nav className="ms-auto">
-//               {userInfo ? (
-//                 <>
-//                   <NavDropdown title={userInfo.name} id="username">
-//                     <LinkContainer to="/profile">
-//                       <NavDropdown.Item>Profile</NavDropdown.Item>
-//                     </LinkContainer>
-//                     <NavDropdown.Item onClick={logoutHandler}>
-//                       Logout
-//                     </NavDropdown.Item>
-//                   </NavDropdown>
-//                 </>
-//               ) : (
-//                 <>
-//                   <LinkContainer to="/login">
-//                     <Nav.Link>
-//                       <FaSignInAlt /> Sign In
-//                     </Nav.Link>
-//                   </LinkContainer>
-//                   <LinkContainer to="/register">
-//                     <Nav.Link>
-//                       <FaSignOutAlt /> Sign Up
-//                     </Nav.Link>
-//                   </LinkContainer>
-//                 </>
-//               )}
-//             </Nav>
-//           </Navbar.Collapse>
-//         </Container>
-//       </Navbar>
-//     </header>
-//   );
-// };
-
-// export default Header;

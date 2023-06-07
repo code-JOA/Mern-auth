e4b09c81f65e8babc34f33f558829ac7c10bf894
Joshua Owusu <joshbillion1@gmail.com>
2023-06-07 13:46:27 +0000

    Commit auto_update

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -19,9 +19,15 @@ const RegisterScreen = () => {
 
   const { userInfo } = useSelector((state) => state.auth);
 
-  const [login, { isLoading }] = useRegisterMutation();[[[[[[[[[';q1111111111111111111111111111111111111111111;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;kkkkkkkkkkkkkkkkkkkkkkkkmdDDDDDDD(9999999iiiiiiiiiiiiiiiiiiiiiiiiiiiio']]]]]]]]]
+  const [login, { isLoading }] = useRegisterMutation();[[[[[[[[[';q1111111111111111111111111111111111111111111;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;kkkkkkkkkkkkkkkkkkkkkkkkmdDDDDDDD(9999999iiiiiiiiiiiiiiiiiiiiiiiiiiiio-']]]]]]]]]]
 
 
+
+
+
+
+
+]
   const submitHandler = async (e) => {
     e.preventDefault();
     console.log("submit");

71446b7ba67635c05545864792bd6580657211a0
Joshua Owusu <joshbillion1@gmail.com>
2023-06-07 13:07:24 +0000

    Commit auto_update

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Form, Button, Col, Row } from "react-bootstrap";
-
+import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from "../components/FormContainer";
 
 const RegisterScreen = () => {

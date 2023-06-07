2ef02cd3281cffcf5763132bdb55c8b201c0eebe
Joshua Owusu <joshbillion1@gmail.com>
2023-06-07 13:08:30 +0000

    Commit auto_update

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Form, Button, Col, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from 'react-redux';

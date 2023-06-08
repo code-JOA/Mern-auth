641b05724154699b13cd8c7ff9919b6476fab3cc
Joshua Owusu <joshbillion1@gmail.com>
2023-06-08 05:27:22 +0000

    Commit auto_update

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from './config/db.js';
 const port = process.env.PORT || 5000; 
 import userRoutes from './routes/UserRoutes.js';
+import PATH from 'PATH';
+
 
 const app = express();
 

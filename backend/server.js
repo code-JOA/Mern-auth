01090d09a1211ce17d96947c7067486d7a951a18
Joshua Owusu <joshbillion1@gmail.com>
2023-06-08 05:36:43 +0000

    Commit auto_update

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,10 @@ app.use(express.urlencoded({extended: true}));
 app.use('./api/users' , userRoutes);
 
 // since we want to be able to deploy our App on the web
+
 if (process.env.NODE_ENV === 'production'){
     const  __dirname= path.resolve();
-    app.use(express.static('path'));
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
 }
 
 app.get('/', (req, res) => res.send('Server is ready'));

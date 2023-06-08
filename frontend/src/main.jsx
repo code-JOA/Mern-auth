52eb3a551b218bf053e878e195931bf12b6f4705
Joshua Owusu <joshbillion1@gmail.com>
2023-06-08 04:34:52 +0000

    Commit auto_update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -22,9 +22,9 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<Homescreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
-      < path="" element={<PrivateRoute />} >
-      <Route path="/profile"  element={<RegisterScreen />} />
-
+      {/* Private Routes */}
+      <Route path="" element={<PrivateRoute />} >
+        <Route path="/profile"  element={<RegisterScreen />} />
       </Route>
     </Route>
   )

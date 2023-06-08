99a2fd431de3b02c9d4ce89a3a1671d5dbe776ba
Joshua Owusu <joshbillion1@gmail.com>
2023-06-08 05:29:30 +0000

    Commit auto_update

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('./api/users' , userRoutes);
 
-if (procress.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production'){
     const  __dirname= path.resolve();
 }
 
